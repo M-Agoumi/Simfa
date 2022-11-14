3b37134ad1628362d3f4da2548b994935d4b599c
Mohamed AGOUMI <agoumihunter@gmail.com>
2022-11-15 00:23:53 +0100

    remove default routes

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,3 @@
   * });
   *
   */
-Simfa\Framework\Router::get('/', [Controller\DefaultController::class, 'index']);
-Simfa\Framework\Router::request('/login', [Controller\DefaultController::class, 'login']);
-Simfa\Framework\Router::get('/logout', [Controller\DefaultController::class, 'logout']);
